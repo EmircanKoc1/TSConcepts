f600f156c9a0898e505824fd5ea09e04f6cf726d
Emircan <emircandv787@gmail.com>
2025-03-04 14:35:56 +0300

    The variable declaration samples have been added.

---
+++ TSConcepts/dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+//the variables 
+function WriteNumber() {
+    // var number = 22;
+    // console.log(number);
+    // let number = 22;
+    // console.log(number);
+    // number = 33;
+    // console.log(number);
+    // const number = 22;
+    // number = 33 ; error
+    // console.log(number);
+}
