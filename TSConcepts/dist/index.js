fc3627a4d2f42c99abdfcfb10f7b615baac28ee4
Emircan <emircandv787@gmail.com>
2025-03-05 21:56:48 +0300

    The generic topics samples have been added.

--- TSConcepts/dist/index.js
+++ TSConcepts/dist/index.js
@@ -344,146 +344,226 @@
 //     console.log(dog);
 // };
 // interfaces();
-function classes() {
-    class Person {
-        get birthdate() {
-            if (this._birthdate != null) {
-                return this._birthdate.toLocaleDateString();
-            }
-            return "The birthdate is unknown!";
+// function classes() {
+//     type Gender = "Female" | "Male";
+//     class Person {
+//         name: string;
+//         public age: number;
+//         private gender: Gender;
+//         private _birthdate?: Date;
+//         get birthdate(): string {
+//             if (this._birthdate != null) {
+//                 return this._birthdate.toLocaleDateString();
+//             }
+//             return "The birthdate is unknown!";
+//         }
+//         set birthdate(date: Date) {
+//             let currentDate = new Date();
+//             if (currentDate >= date) {
+//                 this._birthdate = date;
+//             }
+//         }
+//         constructor(name: string, age: number, gender: Gender) {
+//             this.name = name;
+//             this.age = age;
+//             this.gender = gender;
+//         }
+//         public introduce(): void {
+//             console.log(`Hello, I am ${this.name} , I am ${this.age} years old, and I am a ${this.gender}`);
+//         }
+//     };
+//     const person1: Person = new Person("Emircan", 23, "Female");
+//     person1.introduce();
+//     person1.birthdate = new Date("1999-05-11");
+//     console.log(person1.birthdate);
+//     class MathUtil {
+//         static readonly Pi: number = 3.14;
+//         static InitValue: number = 0;
+//         static readonly SUBTRACT_OPERATOR_SYMBOL: string = "-";
+//     }
+//     console.log(MathUtil.Pi);
+//     // MathUtils.Pi = 22; //err
+//     console.log(MathUtil.InitValue);
+//     MathUtil.InitValue = 33;
+//     console.log(MathUtil.InitValue);
+//     class Shape {
+//         protected area: number = 0;
+//     }
+//     class Rectangle extends Shape {
+//         setArea(area: number): void {
+//             this.area = area;
+//         }
+//     }
+//     abstract class Animal {
+//         abstract makeSound(): void;
+//     }
+//     class Sheep implements Animal {
+//         makeSound(): void {
+//             console.log("meee! meee!!");
+//         }
+//     }
+//     const sheep: Sheep = new Sheep();
+//     sheep.makeSound();
+//     class ProductService {
+//         private readonly _apiAdress: string;
+//         constructor() {
+//             this._apiAdress = "hello";
+//         }
+//         setApiAdress(): void {
+//             // this._apiAdress = "wlnıownwe";//err
+//         }
+//     }
+//     interface IShape {
+//         getArea(): number;
+//     }
+//     class Square implements IShape {
+//         constructor(private widthOrHeight: number) { }
+//         getArea(): number {
+//             return this.widthOrHeight * this.widthOrHeight;
+//         }
+//     }
+//     class Human {
+//         walk(): void {
+//             console.log("The human is walking");
+//         }
+//     }
+//     class FatHuman extends Human {
+//         // constructor(){
+//         //     super();
+//         // }
+//         walk(): void {
+//             console.log("The fat human walking");
+//         }
+//     }
+//     const human1: FatHuman = new FatHuman();
+//     const human2: Human = human1;
+//     human1.walk();
+//     human2.walk();
+//     class First {
+//         write() {
+//             console.log("first");
+//         }
+//     }
+//     class Second {
+//         write() {
+//             console.log("second");
+//         }
+//     }
+//     let variable: First;
+//     variable = new Second();
+//     variable.write();
+//     class Calculator {
+//         add(a: number, b: number): number;
+//         add(a: string, b: string): string;
+//         add(a: boolean, b: boolean): boolean;
+//         add(a: number | string | boolean, b: number | string | boolean): number | string | boolean {
+//             if (typeof a === "number" && typeof b === "number") {
+//                 return a + b;
+//             }
+//             else if (typeof a === "string" && typeof b === "string") {
+//                 return a + b;
+//             } else if (typeof a === "boolean" && typeof b === "boolean") {
+//                 return true;
+//             } else {
+//                 throw new Error("parameters not matched");
+//             }
+//             // add(a: unknown, b: unknown): unknown | never {
+//             //     if (typeof a === "number" && typeof b === "number") {
+//             //         return a + b;
+//             //     }
+//             //     else if (typeof a === "string" && typeof b === "string") {
+//             //         return a + b;
+//             //     } else if (typeof a === "boolean" && typeof b === "boolean") {
+//             //         return true;
+//             //     } else {
+//             //         throw new Error("parameters not matched");
+//             //     }
+//         }
+//     }
+//     const calc = new Calculator();
+//     console.log(calc.add(1, 2));
+//     console.log(calc.add(true, true));
+//     console.log(calc.add("22", "33"));
+//     class Config {
+//         private constructor() { }
+//         private static _config?: Config;
+//         static GetConfig(): Config {
+//             return this._config ??= new Config();
+//         }
+//     }
+// }
+// classes();
+function Generics() {
+    function combine(a, b) {
+        return `${a} - ${b}`;
+    }
+    console.log(combine(1, "emir"));
+    console.log(combine(1, "emir"));
+    class List {
+        constructor() {
+            this._datas = [];
         }
-        set birthdate(date) {
-            let currentDate = new Date();
-            if (currentDate >= date) {
-                this._birthdate = date;
-            }
+        get Length() {
+            return this._datas.length;
         }
-        constructor(name, age, gender) {
-            this.name = name;
-            this.age = age;
-            this.gender = gender;
+        add(value) {
+            this._datas.push(value);
         }
-        introduce() {
-            console.log(`Hello, I am ${this.name} , I am ${this.age} years old, and I am a ${this.gender}`);
+        remove(value) {
+            this._datas = this._datas.filter(data => data !== value);
+        }
+        toArray() {
+            return [...this._datas];
         }
     }
+    const list = new List();
+    list.add(22);
+    list.add(33);
+    list.add(44);
+    list.add(44);
+    list.add(44);
+    console.log(list.Length);
+    console.log(list.toArray());
+    list.remove(44);
+    console.log(list.toArray());
     ;
-    const person1 = new Person("Emircan", 23, "Female");
-    person1.introduce();
-    person1.birthdate = new Date("1999-05-11");
-    console.log(person1.birthdate);
-    class MathUtil {
+    const keyValuePair = {
+        key: 1,
+        value: 1
+    };
+    const nullOrNumber = { value: 2020 };
+    console.log(nullOrNumber.value);
+    ;
+    function writePersonDetails(value) {
+        console.log(value.name, "-", value.age);
     }
-    MathUtil.Pi = 3.14;
-    MathUtil.InitValue = 0;
-    MathUtil.SUBTRACT_OPERATOR_SYMBOL = "-";
-    console.log(MathUtil.Pi);
-    // MathUtils.Pi = 22; //err
-    console.log(MathUtil.InitValue);
-    MathUtil.InitValue = 33;
-    console.log(MathUtil.InitValue);
-    class Shape {
-        constructor() {
-            this.area = 0;
-        }
+    const person1 = {
+        name: "Emircan",
+        age: 23
+    };
+    const person2 = {
+        name: "Mahmut",
+        age: 33
+    };
+    writePersonDetails(person1);
+    writePersonDetails(person2);
+    // writePersonDetails({ surname: "xxxx" }) //The object is not a person //error
+    console.log();
+    const key = "age";
+    console.log(key);
+    function GetDetail(obj, key) {
+        return obj[key];
     }
-    class Rectangle extends Shape {
-        setArea(area) {
-            this.area = area;
-        }
-    }
-    class Animal {
-    }
-    class Sheep {
-        makeSound() {
-            console.log("meee! meee!!");
-        }
-    }
-    const sheep = new Sheep();
-    sheep.makeSound();
-    class ProductService {
-        constructor() {
-            this._apiAdress = "hello";
-        }
-        setApiAdress() {
-            // this._apiAdress = "wlnıownwe";//err
-        }
-    }
-    class Square {
-        constructor(widthOrHeight) {
-            this.widthOrHeight = widthOrHeight;
-        }
-        getArea() {
-            return this.widthOrHeight * this.widthOrHeight;
-        }
-    }
-    class Human {
-        walk() {
-            console.log("The human is walking");
-        }
-    }
-    class FatHuman extends Human {
-        // constructor(){
-        //     super();
-        // }
-        walk() {
-            console.log("The fat human walking");
-        }
-    }
-    const human1 = new FatHuman();
-    const human2 = human1;
-    human1.walk();
-    human2.walk();
-    class First {
-        write() {
-            console.log("first");
-        }
-    }
-    class Second {
-        write() {
-            console.log("second");
-        }
-    }
-    let variable;
-    variable = new Second();
-    variable.write();
-    class Calculator {
-        add(a, b) {
-            if (typeof a === "number" && typeof b === "number") {
-                return a + b;
-            }
-            else if (typeof a === "string" && typeof b === "string") {
-                return a + b;
-            }
-            else if (typeof a === "boolean" && typeof b === "boolean") {
-                return true;
-            }
-            else {
-                throw new Error("parameters not matched");
-            }
-            // add(a: unknown, b: unknown): unknown | never {
-            //     if (typeof a === "number" && typeof b === "number") {
-            //         return a + b;
-            //     }
-            //     else if (typeof a === "string" && typeof b === "string") {
-            //         return a + b;
-            //     } else if (typeof a === "boolean" && typeof b === "boolean") {
-            //         return true;
-            //     } else {
-            //         throw new Error("parameters not matched");
-            //     }
-        }
-    }
-    const calc = new Calculator();
-    console.log(calc.add(1, 2));
-    console.log(calc.add(true, true));
-    console.log(calc.add("22", "33"));
-    class Config {
-        constructor() { }
-        static GetConfig() {
-            var _a;
-            return (_a = this._config) !== null && _a !== void 0 ? _a : (this._config = new Config());
-        }
+    let dog = {
+        name: "josef",
+        height: 1.11
+    };
+    console.log(GetDetail(dog, "height"));
+    console.log(GetDetail(dog, "name"));
+    const res1 = { value: "emir" };
+    const res2 = { value: "emir" };
+    function merge(value1, value2) {
+        return Object.assign(Object.assign({}, value1), value2);
     }
 }
-classes();
+Generics();
--- TSConcepts/src/index.ts
+++ TSConcepts/src/index.ts
@@ -530,230 +530,371 @@
 
 
 
-function classes() {
+// function classes() {
 
-    type Gender = "Female" | "Male";
+//     type Gender = "Female" | "Male";
 
-    class Person {
+//     class Person {
 
-        name: string;
-        public age: number;
-        private gender: Gender;
-        private _birthdate?: Date;
+//         name: string;
+//         public age: number;
+//         private gender: Gender;
+//         private _birthdate?: Date;
 
-        get birthdate(): string {
+//         get birthdate(): string {
 
-            if (this._birthdate != null) {
-                return this._birthdate.toLocaleDateString();
-            }
-            return "The birthdate is unknown!";
+//             if (this._birthdate != null) {
+//                 return this._birthdate.toLocaleDateString();
+//             }
+//             return "The birthdate is unknown!";
+//         }
+
+//         set birthdate(date: Date) {
+//             let currentDate = new Date();
+
+//             if (currentDate >= date) {
+//                 this._birthdate = date;
+//             }
+
+//         }
+
+
+
+//         constructor(name: string, age: number, gender: Gender) {
+//             this.name = name;
+//             this.age = age;
+//             this.gender = gender;
+//         }
+
+//         public introduce(): void {
+//             console.log(`Hello, I am ${this.name} , I am ${this.age} years old, and I am a ${this.gender}`);
+//         }
+
+//     };
+
+//     const person1: Person = new Person("Emircan", 23, "Female");
+//     person1.introduce();
+//     person1.birthdate = new Date("1999-05-11");
+//     console.log(person1.birthdate);
+
+//     class MathUtil {
+//         static readonly Pi: number = 3.14;
+//         static InitValue: number = 0;
+//         static readonly SUBTRACT_OPERATOR_SYMBOL: string = "-";
+//     }
+
+
+//     console.log(MathUtil.Pi);
+//     // MathUtils.Pi = 22; //err
+//     console.log(MathUtil.InitValue);
+//     MathUtil.InitValue = 33;
+//     console.log(MathUtil.InitValue);
+
+
+//     class Shape {
+//         protected area: number = 0;
+//     }
+
+//     class Rectangle extends Shape {
+
+//         setArea(area: number): void {
+//             this.area = area;
+//         }
+
+//     }
+
+//     abstract class Animal {
+//         abstract makeSound(): void;
+//     }
+
+//     class Sheep implements Animal {
+//         makeSound(): void {
+//             console.log("meee! meee!!");
+
+//         }
+
+//     }
+
+
+
+//     const sheep: Sheep = new Sheep();
+//     sheep.makeSound();
+
+//     class ProductService {
+//         private readonly _apiAdress: string;
+
+//         constructor() {
+//             this._apiAdress = "hello";
+//         }
+
+//         setApiAdress(): void {
+//             // this._apiAdress = "wlnıownwe";//err
+//         }
+//     }
+
+//     interface IShape {
+//         getArea(): number;
+//     }
+
+//     class Square implements IShape {
+
+//         constructor(private widthOrHeight: number) { }
+
+//         getArea(): number {
+//             return this.widthOrHeight * this.widthOrHeight;
+//         }
+
+//     }
+
+
+//     class Human {
+//         walk(): void {
+//             console.log("The human is walking");
+
+//         }
+//     }
+
+//     class FatHuman extends Human {
+
+//         // constructor(){
+//         //     super();
+//         // }
+
+//         walk(): void {
+//             console.log("The fat human walking");
+//         }
+
+//     }
+
+//     const human1: FatHuman = new FatHuman();
+//     const human2: Human = human1;
+//     human1.walk();
+//     human2.walk();
+
+//     class First {
+//         write() {
+//             console.log("first");
+//         }
+//     }
+
+//     class Second {
+//         write() {
+//             console.log("second");
+
+//         }
+//     }
+
+//     let variable: First;
+//     variable = new Second();
+//     variable.write();
+
+
+//     class Calculator {
+
+//         add(a: number, b: number): number;
+//         add(a: string, b: string): string;
+//         add(a: boolean, b: boolean): boolean;
+
+
+//         add(a: number | string | boolean, b: number | string | boolean): number | string | boolean {
+
+//             if (typeof a === "number" && typeof b === "number") {
+//                 return a + b;
+//             }
+//             else if (typeof a === "string" && typeof b === "string") {
+//                 return a + b;
+//             } else if (typeof a === "boolean" && typeof b === "boolean") {
+//                 return true;
+//             } else {
+//                 throw new Error("parameters not matched");
+//             }
+
+
+//             // add(a: unknown, b: unknown): unknown | never {
+
+//             //     if (typeof a === "number" && typeof b === "number") {
+//             //         return a + b;
+//             //     }
+//             //     else if (typeof a === "string" && typeof b === "string") {
+//             //         return a + b;
+//             //     } else if (typeof a === "boolean" && typeof b === "boolean") {
+//             //         return true;
+//             //     } else {
+//             //         throw new Error("parameters not matched");
+//             //     }
+
+//         }
+
+
+//     }
+
+//     const calc = new Calculator();
+//     console.log(calc.add(1, 2));
+//     console.log(calc.add(true, true));
+//     console.log(calc.add("22", "33"));
+
+
+//     class Config {
+
+//         private constructor() { }
+
+//         private static _config?: Config;
+
+//         static GetConfig(): Config {
+
+//             return this._config ??= new Config();
+
+//         }
+
+//     }
+
+
+
+
+// }
+
+
+// classes();
+
+
+
+function Generics() {
+
+    function combine<T, U>(a: T, b: U) {
+
+        return `${a} - ${b}`;
+    }
+
+
+    console.log(combine<number, string>(1, "emir"));
+    console.log(combine(1, "emir"));
+
+    class List<T> {
+
+        private _datas: T[] = [];
+
+
+        get Length(): number {
+            return this._datas.length;
+        }
+        add(value: T): void {
+            this._datas.push(value);
+
         }
 
-        set birthdate(date: Date) {
-            let currentDate = new Date();
-
-            if (currentDate >= date) {
-                this._birthdate = date;
-            }
+        remove(value: T): void {
+            this._datas = this._datas.filter(data => data !== value);
 
         }
-
-
-
-        constructor(name: string, age: number, gender: Gender) {
-            this.name = name;
-            this.age = age;
-            this.gender = gender;
+        toArray(): T[] {
+            return [...this._datas];
         }
 
-        public introduce(): void {
-            console.log(`Hello, I am ${this.name} , I am ${this.age} years old, and I am a ${this.gender}`);
-        }
+    }
 
+    const list = new List<number>();
+
+    list.add(22);
+    list.add(33);
+    list.add(44);
+    list.add(44);
+    list.add(44);
+
+    console.log(list.Length);
+    console.log(list.toArray());
+
+    list.remove(44);
+
+    console.log(list.toArray());
+
+    interface KeyValuePair<T1, T2> {
+        key: T1,
+        value: T2
     };
 
-    const person1: Person = new Person("Emircan", 23, "Female");
-    person1.introduce();
-    person1.birthdate = new Date("1999-05-11");
-    console.log(person1.birthdate);
+    const keyValuePair: KeyValuePair<number, number> = {
+        key: 1,
+        value: 1
+    };
 
-    class MathUtil {
-        static readonly Pi: number = 3.14;
-        static InitValue: number = 0;
-        static readonly SUBTRACT_OPERATOR_SYMBOL: string = "-";
-    }
+    type Nullable<T> = {
+        value: T | null
+    };
 
+    const nullOrNumber: Nullable<number> = { value: 2020 };
 
-    console.log(MathUtil.Pi);
-    // MathUtils.Pi = 22; //err
-    console.log(MathUtil.InitValue);
-    MathUtil.InitValue = 33;
-    console.log(MathUtil.InitValue);
+    console.log(nullOrNumber.value);
 
 
-    class Shape {
-        protected area: number = 0;
-    }
+    interface Person {
+        name: string,
+        age: number
+    };
 
-    class Rectangle extends Shape {
+    function writePersonDetails<T extends Person>(value: T) {
 
-        setArea(area: number): void {
-            this.area = area;
-        }
+        console.log(value.name, "-", value.age);
 
     }
 
-    abstract class Animal {
-        abstract makeSound(): void;
+    const person1: Person = {
+        name: "Emircan",
+        age: 23
+    };
+
+    const person2: Person = {
+        name: "Mahmut",
+        age: 33
+    };
+
+
+    writePersonDetails(person1);
+    writePersonDetails(person2);
+    // writePersonDetails({ surname: "xxxx" }) //The object is not a person //error
+
+
+    console.log();
+
+    type PersonKeys = keyof Person;
+
+    const key: PersonKeys = "age";
+
+    console.log(key);
+
+    function GetDetail<T, K extends keyof T>(obj: T, key: K): T[K] {
+        return obj[key];
     }
 
-    class Sheep implements Animal {
-        makeSound(): void {
-            console.log("meee! meee!!");
+    type Animal = {
+        name: string,
+        height: number
+    };
 
-        }
+    let dog: Animal = {
+        name: "josef",
+        height: 1.11
+    };
 
+    console.log(GetDetail<Animal, keyof Animal>(dog, "height"));
+    console.log(GetDetail<Animal, keyof Animal>(dog, "name"));
+
+    type Response<T = unknown> = {
+        value: T
+    };
+
+    const res1: Response = { value: "emir" };
+    const res2: Response<string> = { value: "emir" };
+
+
+    function merge<T1, T2>(value1: T1, value2: T2): T1 & T2 {
+        return { ...value1, ...value2 };
     }
-
-
-
-    const sheep: Sheep = new Sheep();
-    sheep.makeSound();
-
-    class ProductService {
-        private readonly _apiAdress: string;
-
-        constructor() {
-            this._apiAdress = "hello";
-        }
-
-        setApiAdress(): void {
-            // this._apiAdress = "wlnıownwe";//err
-        }
-    }
-
-    interface IShape {
-        getArea(): number;
-    }
-
-    class Square implements IShape {
-
-        constructor(private widthOrHeight: number) { }
-
-        getArea(): number {
-            return this.widthOrHeight * this.widthOrHeight;
-        }
-
-    }
-
-
-    class Human {
-        walk(): void {
-            console.log("The human is walking");
-
-        }
-    }
-
-    class FatHuman extends Human {
-
-        // constructor(){
-        //     super();
-        // }
-
-        walk(): void {
-            console.log("The fat human walking");
-        }
-
-    }
-
-    const human1: FatHuman = new FatHuman();
-    const human2: Human = human1;
-    human1.walk();
-    human2.walk();
-
-    class First {
-        write() {
-            console.log("first");
-        }
-    }
-
-    class Second {
-        write() {
-            console.log("second");
-
-        }
-    }
-
-    let variable: First;
-    variable = new Second();
-    variable.write();
-
-
-    class Calculator {
-
-        add(a: number, b: number): number;
-        add(a: string, b: string): string;
-        add(a: boolean, b: boolean): boolean;
-
-
-        add(a: number | string | boolean, b: number | string | boolean): number | string | boolean {
-
-            if (typeof a === "number" && typeof b === "number") {
-                return a + b;
-            }
-            else if (typeof a === "string" && typeof b === "string") {
-                return a + b;
-            } else if (typeof a === "boolean" && typeof b === "boolean") {
-                return true;
-            } else {
-                throw new Error("parameters not matched");
-            }
-
-
-            // add(a: unknown, b: unknown): unknown | never {
-
-            //     if (typeof a === "number" && typeof b === "number") {
-            //         return a + b;
-            //     }
-            //     else if (typeof a === "string" && typeof b === "string") {
-            //         return a + b;
-            //     } else if (typeof a === "boolean" && typeof b === "boolean") {
-            //         return true;
-            //     } else {
-            //         throw new Error("parameters not matched");
-            //     }
-
-        }
-
-
-    }
-
-    const calc = new Calculator();
-    console.log(calc.add(1, 2));
-    console.log(calc.add(true, true));
-    console.log(calc.add("22", "33"));
-
-
-    class Config {
-
-        private constructor() { }
-
-        private static _config?: Config;
-
-        static GetConfig(): Config {
-
-            return this._config ??= new Config();
-
-        }
-
-    }
-
 
 
 
 }
 
 
-classes();
+Generics();
 
 
