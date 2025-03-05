dfbff7f22a4afafcab35fd917d2f791ea43a5a88
Emircan <emircandv787@gmail.com>
2025-03-05 21:38:20 +0300

    The var, let, and const keyword samples have been added.

--- TSConcepts/dist/index.js
+++ TSConcepts/dist/index.js
@@ -1,13 +1,12 @@
 "use strict";
-//the variables 
 function WriteNumber() {
-    // var number = 22;
-    // console.log(number);
-    // let number = 22;
-    // console.log(number);
-    // number = 33;
-    // console.log(number);
-    // const number = 22;
-    // number = 33 ; error
-    // console.log(number);
+    var number1 = 22;
+    console.log(number1);
+    let number2 = 22;
+    console.log(number2);
+    number2 = 33;
+    console.log(number2);
+    const number3 = 22;
+    // number3 = 33 ; //error
+    console.log(number3);
 }
