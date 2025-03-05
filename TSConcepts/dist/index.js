6202493491a0f7588a704d26ab2a6f65b2ba596c
Emircan <emircandv787@gmail.com>
2025-03-05 21:44:36 +0300

    The data type samples have been added.

--- TSConcepts/dist/index.js
+++ TSConcepts/dist/index.js
@@ -1,12 +1,63 @@
 "use strict";
-function WriteNumber() {
-    var number1 = 22;
-    console.log(number1);
-    let number2 = 22;
-    console.log(number2);
-    number2 = 33;
-    console.log(number2);
-    const number3 = 22;
-    // number3 = 33 ; //error
-    console.log(number3);
-}
+// function WriteNumber() {
+//     var number1 = 22;
+//     console.log(number1);
+//     let number2 = 22;
+//     console.log(number2);
+//     number2 = 33;
+//     console.log(number2);
+//     const number3 = 22;
+//     // number3 = 33 ; //error
+//     console.log(number3);
+// }
+// function dataTypes() {
+//     let name: string = "James";
+//     console.log(name);
+//     let price: number = 3.14;
+//     console.log(price);
+//     price = 3;
+//     console.log(price);
+//     let isGreater: boolean = true;
+//     console.log(isGreater);
+//     let iAmNull: null = null;
+//     console.log(iAmNull);
+//     let iAmUndefined;
+//     console.log(iAmUndefined);
+//     iAmUndefined = undefined;
+//     console.log(iAmUndefined);
+//     let everything: any = 22;
+//     console.log(everything);
+//     everything = "Hello";
+//     console.log(everything);
+//     everything = undefined;
+//     console.log(everything);
+//     everything = null;
+//     console.log(everything);
+//     let number: number = 0;
+//     function incrementNumber(): void {
+//         number++;
+//     }
+//     incrementNumber();
+//     incrementNumber();
+//     incrementNumber();
+//     console.log(number);
+//     function throwErr(): never {
+//         throw new Error("This is an error");
+//     }
+//     function runEveryTime(): never {
+//         while (true) {
+//             console.log("Hello");
+//         }
+//     }
+//     // throwErr();
+//     // runEveryTime();
+//     let maybeOtherDataTypes: unknown = "Hello";
+//     // maybeOtherDataTypes = 22;
+//     if (typeof maybeOtherDataTypes === "string") {
+//         maybeOtherDataTypes = maybeOtherDataTypes.toUpperCase();
+//     } else if (typeof maybeOtherDataTypes === "number") {
+//         maybeOtherDataTypes = 33;
+//     }
+//     console.log(maybeOtherDataTypes);
+// }
+// dataTypes();
